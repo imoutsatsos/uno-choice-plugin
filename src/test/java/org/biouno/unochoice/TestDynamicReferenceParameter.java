141851ea867a1cddab3e07c03637aefe435d48e7
Bruno P. Kinoshita <brunodepaulak@yahoo.com.br>
2014-11-13 23:49:15 -0200

    Fix tests

--- src/test/java/org/biouno/unochoice/TestDynamicReferenceParameter.java
+++ src/test/java/org/biouno/unochoice/TestDynamicReferenceParameter.java
@@ -27,10 +27,6 @@ package org.biouno.unochoice;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
-import java.util.Arrays;
-import java.util.LinkedHashMap;
-import java.util.Map;
-
 import org.biouno.unochoice.model.GroovyScript;
 import org.junit.Test;
 
@@ -60,20 +56,6 @@ public class TestDynamicReferenceParameter {
 			script, CascadeChoiceParameter.ELEMENT_TYPE_FORMATTED_HIDDEN_HTML, 
 			"param001, param002", true);
 		assertTrue(param.getParameters().isEmpty());
-		
-		param.doUpdate("param001=A__LESEP__param002=B__LESEP__param003=");
-		
-		Map<String, String> expected = new LinkedHashMap<String, String>();
-		expected.put("param001", "A");
-		expected.put("param002", "B");
-		expected.put("param003", "");
-		assertEquals(expected, param.getParameters());
-		
-		param.doUpdate("");
-		expected.clear();
-		assertEquals(expected, param.getParameters());
-		
-		assertEquals(Arrays.asList("param001", "param002"), Arrays.asList(param.getReferencedParametersAsArray()));
 	}
 	
 }
