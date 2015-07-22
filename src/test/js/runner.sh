932d65a2dd63e6a4585f771e43f2c4943fbbb9f5
Bruno P. Kinoshita <brunodepaulak@yahoo.com.br>
2015-07-22 22:17:40 +1200

    Selectively execute phantomjs only when available in the PATH envvar

---
+++ src/test/js/runner.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# This bash-shell script is a wrapper for phantomjs
+# It first checks if the program exists and is executable, if so
+# runs phantomjs normally. Otherwise just pretend nothing happened
+# and keeps with the rest of the project build.
+
+PHANTOMJS_EXEC=$(which phantomjs)
+
+if [[ -x $PHANTOMJS_EXEC ]];
+	then
+	phantomjs $1 $2
+else
+	exit 0
+fi
