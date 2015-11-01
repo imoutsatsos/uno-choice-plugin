b4d3c69ce39a68de1ef02ae079f3675b1eabecf8
lyenliang <i745m@hotmail.com>
2015-11-01 21:53:52 +0800

    Update runner.bat

--- src/test/js/runner.bat
+++ src/test/js/runner.bat
@@ -1,4 +1,4 @@
-REM This bash-shell script is a wrapper for phantomjs
+REM This batch script is a wrapper for phantomjs
 REM It first checks if the program exists and is executable, if so
 REM runs phantomjs normally. Otherwise just pretend nothing happened
 REM and keeps with the rest of the project build.
