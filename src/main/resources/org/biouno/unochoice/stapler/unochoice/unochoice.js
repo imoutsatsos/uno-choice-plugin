650ed9799efbad7e97e7764bbcaef0adee8f145b
Bruno P. Kinoshita <kinow@users.noreply.github.com>
2021-08-20 15:10:31 +1200

    [JENKINS-66411] Update license year

--- src/main/resources/org/biouno/unochoice/stapler/unochoice/unochoice.js
+++ src/main/resources/org/biouno/unochoice/stapler/unochoice/unochoice.js
@@ -1,7 +1,7 @@
 /**
  * The MIT License (MIT)
  *
- * Copyright (c) 2014-2020 Ioannis Moutsatsos, Bruno P. Kinoshita
+ * Copyright (c) 2014-2021 Ioannis Moutsatsos, Bruno P. Kinoshita
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
