94c7e39bb3dac2f334e99a68fa8109f4db3b7d15
Bruno P. Kinoshita <kinow@users.noreply.github.com>
2021-08-20 18:19:09 +1200

    [JENKINS-62835] Remove unused imports

--- src/main/java/org/biouno/unochoice/model/ScriptlerScript.java
+++ src/main/java/org/biouno/unochoice/model/ScriptlerScript.java
@@ -24,11 +24,8 @@
 
 package org.biouno.unochoice.model;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.LinkedHashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
